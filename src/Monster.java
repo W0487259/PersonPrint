a70b912fae17ecfaf2599a84d34103af2a229f05
Evan <w0487259@campus.nscc.ca>
2024-02-28 13:47:18 -0400

    Added monster class

--- src/Monster.java
+++ src/Monster.java
@@ -3,12 +3,10 @@ public class Monster {
     private String ssn;
 
 
-
     public Monster(String name, String ssn) {
         this.name = name;
         this.ssn = ssn;
     }
-
 
     public String getName() {
         return this.name;
